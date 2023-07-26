ee7e9bacc63d3517b04757a0cee6325348aceefe
GrigArakelyan <araqelyan461@gmail.com>
2023-07-26 15:06:45 +0400

    project test

--- src/Components/TimePickerEnd.jsx
+++ src/Components/TimePickerEnd.jsx
@@ -1,3 +1,4 @@
+import "./TimePicker.scss"
 import * as React from 'react';
 import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
